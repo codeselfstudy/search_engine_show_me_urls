2727e05193272f2a07799153c0e54793a43f0602
J. Cohen <hostelmarketing@gmail.com>
2020-01-20 16:18:33 -0800

    First working version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Google Show Me URLs
 
-Bring URLs back to Google's search results pages.
+Google stopped showing URLs in the search engine results, making it difficult to understand what you're clicking on. It's hard to find specific sites from a description of the page in the `title` elements.
+
+This browser extension brings URLs back to Google's search results pages so that you can safely see where you're going.
--- src/content_script.js
+++ src/content_script.js
@@ -1,23 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
+// prevent adding the links multiple times
+var addedLinks = document.querySelector(".show-me-urls");
+if (!addedLinks) {
     fixGoogle();
-});
+}
 
+/**
+ * The main functionality.
+ */
 function fixGoogle() {
     const els = document.querySelectorAll("#search h2 + div [href]");
     els.forEach(el => {
-        if (el.innerText.trim() && !el.href.match(/google\.com/)) {
+        if (
+            // remove empty elements
+            el.innerText.trim() &&
+            // remove internal links
+            !el.href.match(/google\.com/) &&
+            // don't append URLs to images
+            !isImg(el)
+        ) {
             const a = createLink(el);
             el.parentNode.insertBefore(a, el.nextSibling);
         }
     });
 }
 
+/**
+ * See if the link is wrapped around an image.
+ */
+function isImg(el) {
+    const childTags = [...el.childNodes].map(x => x.tagName);
+    if (childTags.includes("G-IMG")) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Create a link to display on the page.
+ */
 function createLink(el) {
     const div = document.createElement("div");
+    div.classList.add("show-me-urls");
     const a = document.createElement("a");
     a.href = el.href;
     a.innerText = el.href;
     div.appendChild(a);
     a.style.backgroundColor = "#fffbd6";
+    a.style.fontSize = "1.17rem";
     return div;
 }
